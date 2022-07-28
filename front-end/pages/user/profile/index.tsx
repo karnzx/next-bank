109ef55f75532cf16a0065a813af43fd7915b315
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2022-07-29 01:31:37 +0700

    feat: 🎸 add actions button

--- front-end/pages/user/profile/index.tsx
+++ front-end/pages/user/profile/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { AccountInfoType, getAccountInfo } from "../../../helper/account";
 
@@ -12,22 +13,50 @@ const Profile: NextPage = () => {
 
   return (
     <>
-      <div className="flex justify-center border-2 border-blue-900 shadow-lg rounded-md md:mx-20">
-        <div className="flex flex-col p-5 w-full pl-10">
-          <h1 className="text-[3rem] font-semi-bold under text-blue-700">
+      <div className="flex justify-center">
+        <div className="p-10 items-center border border-blue-900 shadow-lg rounded-md">
+          <h1 className="text-[3rem] font-bold under text-blue-700 text-center">
             Profile
           </h1>
-          <hr className="horizontal" />
+          <hr className="border-4 border-blue-200" />
           <div>
             {user ? (
               <div>
-                <div className="text-[2rem] md:flex gap-3">
-                  <h2>username: </h2>
-                  <h2>{user.username}</h2>
+                <div className="userData">
+                  <div className="text-[2rem] flex gap-3">
+                    <h2>username: </h2>
+                    <h2>{user.username}</h2>
+                  </div>
+                  <div className="text-[2rem] flex gap-3">
+                    <h2>balance: </h2>
+                    <h2>{user.balance} $</h2>
+                  </div>
                 </div>
-                <div className="text-[2rem] md:flex gap-3">
-                  <h2>balance: </h2>
-                  <h2>{user.balance} $</h2>
+                <hr className="pb-2" />
+                <div className="actions py-5">
+                  <div className="md:flex md:justify-center grid gap-5">
+                    <div>
+                      <Link href="/user/deposit">
+                        <a className="capitalize bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                          deposit
+                        </a>
+                      </Link>
+                    </div>
+                    <div>
+                      <Link href="/user/withdraw">
+                        <a className="capitalize bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                          withdraw
+                        </a>
+                      </Link>
+                    </div>
+                    <div>
+                      <Link href="/user/transfer">
+                        <a className="capitalize bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                          transfer
+                        </a>
+                      </Link>
+                    </div>
+                  </div>
                 </div>
               </div>
             ) : (

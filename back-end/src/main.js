235ecd244b13a5befcac1ccd5e675cba55aa2a69
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2022-07-28 17:24:18 +0700

    feat: 🎸 add login api

--- back-end/src/main.js
+++ back-end/src/main.js
@@ -10,6 +10,7 @@ const models = require('./db')
 
 // this secret only use for development use(require('crypto').randomBytes(64).toString('hex'))
 const TOKEN_SECRET = config.TOKEN_SECRET
+const TOKEN_EXPIRES_IN = '1h'
 const app = express()
 const port = process.env['SERVER_PORT'] || 5000
 
@@ -17,6 +18,40 @@ app.use(cors())
 app.use('/img', express.static('public'))
 app.use(expressLogger);
 app.use(bodyParser.json())
+
+app.post('/api/login', async (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if (!username || !password) {
+        res.sendStatus(400).send("User or Password not provided")
+        return
+    }
+
+    const sendInvalidUserPassword = () => {
+        res.status(401).send('Invalid user or Password')
+    }
+    // search username in db
+    let user = await models.User.findOne({ username: username }).exec()
+    if (user == null) {
+        sendInvalidUserPassword()
+        return
+    }
+    user.comparePassword(password, function (err, isMatch) {
+        if (err) {
+            logger.error(err);
+            res.status(500).send()
+        }
+        // console.log(user, isMatch);
+        if (!isMatch) {
+            sendInvalidUserPassword()
+            return
+        }
+        logger.info(`user: ${user.id} grant access token`)
+        let access_token = jwt.sign({ id: user._id }, TOKEN_SECRET, { expiresIn: TOKEN_EXPIRES_IN })
+        res.send({ access_token })
+    })
+})
 
 // health check
 app.get('/api/ping', (req, res) => {

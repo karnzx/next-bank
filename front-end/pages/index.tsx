e39d270623848c1a9241c42bd87d82e20a9acee3
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2022-07-29 16:15:59 +0700

    edit css

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from "next";
-import Link from 'next/link'
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-
-    <div className="bg-fixed bg-cover opacity-90" style={{ backgroundImage: `url("http://localhost:3000/imgs/pexels-ivan-samkov-7621142.jpg")` }} >
+    <div className="bg-fixed bg-cover opacity-90 main-bg-img">
       <main className="flex flex-col justify-center items-center min-h-screen">
-        <h1 className="text-8xl text-transparent font-extrabold bg-clip-text bg-gradient-to-tl from-purple-100 to-indigo-100">Next Bank</h1>
+        <h1 className="text-8xl text-transparent font-extrabold bg-clip-text bg-gradient-to-tl from-purple-100 to-indigo-100">
+          Next Bank
+        </h1>
 
         <p className="my-6 leading-6 text-3xl text-white font-mono">
           <Link href="/about">

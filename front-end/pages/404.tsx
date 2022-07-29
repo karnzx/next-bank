cad0fd8a299d41687d66e21364cb621679252655
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2022-07-29 13:03:57 +0700

    feat: 🎸 404 page

---
+++ front-end/pages/404.tsx
@@ -0,0 +1,11 @@
+import { warn } from "console";
+
+const Custom404 = () => {
+  return (
+    <div className="flex justify-center items-center h-full min-h-screen">
+      <h1 className="text-[5rem]">404 | Page Not Found</h1>
+    </div>
+  );
+}
+
+export default Custom404;
